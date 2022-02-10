71292fa1332f220ca8ab50f30c434da5e5776ec9
CheonGyeHwan <cgh5531@naver.com>
2022-02-10 16:26:19 +0900

    spring boot # 연습

--- myapplication/src/test/java/ex04/MyApplicationTest.java
+++ myapplication/src/test/java/ex04/MyApplicationTest.java
@@ -1,15 +1,13 @@
-package ex01;
+package ex04;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ex04.component.MyComponent;
 
-//@ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes= {MyApplication.class})
-/* @Configuration 설정 클래스는 자동(스캔)으로 찾지 못함 */
 @SpringBootTest
-public class MyApplicationTest02 {
+public class MyApplicationTest {
 	@Autowired
 	private MyComponent myComponent;
 	
